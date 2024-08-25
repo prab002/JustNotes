04c3b271851da0797480c5f862735d2ec128b583
prab002 <mprabhanjan18@gmail.com>
2024-08-25 22:48:06 +0530

    added user setting

--- app/(main)/(routes)/document/page.tsx
+++ app/(main)/(routes)/document/page.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
 
 function Document() {
-  return <div>details lcndlakcdnc dlkc dkcd mck;dm c;dmc </div>;
+  const { user } = useUser();
+  console.log(user);
+  return (
+    <div className="w-full h-full flex items-center justify-center flex-col space-y-4">
+      <Image
+        src="/empty.jpg"
+        width={300}
+        height={300}
+        alt="empty "
+        className="dark:hidden"
+      />
+      <h1 className="text-lg font-medium">
+        welcome to {user?.username}&apos;'s JustNote{" "}
+      </h1>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
 }
 
 export default Document;
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,20 +5,21 @@ import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import UserIteam from "./user-item";
 
 const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
 
   const isResizeRef = useRef(false);
-  const sidebarRef = useRef<ElementRef<"aside">>(null);
-  const navbarRef = useRef<ElementRef<"div">>(null);
+  const sidebarRef = useRef<HTMLElement>(null);
+  const navbarRef = useRef<HTMLDivElement>(null);
   const [isResetting, setIsResetting] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
   useEffect(() => {
     if (isMobile) {
-      collaspe();
+      collapse();
     } else {
       resetWidth();
     }
@@ -26,9 +27,10 @@ const Navigation = () => {
 
   useEffect(() => {
     if (isMobile) {
-      collaspe();
+      collapse();
     }
   }, [pathname, isMobile]);
+
   const handleMouseDown = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
@@ -72,18 +74,20 @@ const Navigation = () => {
     if (sidebarRef.current && navbarRef.current) {
       setIsCollapsed(false);
       setIsResetting(true);
-    }
 
-    sidebarRef.current.style.width = isMobile ? "100%" : "240px";
-    navbarRef.current.style.setProperty(
-      "width",
-      isMobile ? "0" : "calc(100% - 240px)"
-    );
-    navbarRef.current.style.setProperty("left", isMobile ? "100%" : "240px");
-    setTimeout(() => setIsResetting(false), 300);
+      const sidebarWidth = isMobile ? "100%" : "240px";
+      sidebarRef.current.style.width = sidebarWidth;
+      navbarRef.current.style.setProperty(
+        "width",
+        isMobile ? "100%" : `calc(100% - 240px)`
+      );
+      navbarRef.current.style.setProperty("left", isMobile ? "0" : "240px");
+
+      setTimeout(() => setIsResetting(false), 300);
+    }
   };
 
-  const collaspe = () => {
+  const collapse = () => {
     if (sidebarRef.current && navbarRef.current) {
       setIsCollapsed(true);
       setIsResetting(true);
@@ -91,12 +95,13 @@ const Navigation = () => {
       sidebarRef.current.style.width = "0";
       navbarRef.current.style.setProperty("width", "100%");
       navbarRef.current.style.setProperty("left", "0");
+
       setTimeout(() => setIsResetting(false), 300);
     }
   };
 
   return (
-    <>
+    <div className="relative h-full">
       <aside
         ref={sidebarRef}
         className={cn(
@@ -106,7 +111,7 @@ const Navigation = () => {
         )}
       >
         <div
-          onClick={collaspe}
+          onClick={collapse}
           role="button"
           className={cn(
             "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
@@ -116,7 +121,7 @@ const Navigation = () => {
           <ChevronsLeft className="h-6 w-6" />
         </div>
         <div>
-          <p>Action Items</p>
+          <UserIteam />
         </div>
         <div className="mt-4">
           <p>Documents</p>
@@ -130,9 +135,9 @@ const Navigation = () => {
       <div
         ref={navbarRef}
         className={cn(
-          "absolute top-0 z-[9999] bg-background dark:bg-[#1f1f1f] w-[calc(100%-240px)] h-full",
+          "absolute top-0 z-[9999] bg-background dark:bg-[#1f1f1f] h-full",
           isResetting && "transition-all ease-in-out duration-300",
-          isMobile && "left-0 w-full "
+          isMobile && "left-0 w-full"
         )}
       >
         <nav className="bg-transparent px-3 py-2 w-full">
@@ -145,7 +150,7 @@ const Navigation = () => {
           )}
         </nav>
       </div>
-    </>
+    </div>
   );
 };
 
