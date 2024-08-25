855278f6a0536f2ea5c5ba223dcf6c9e977d2fd2
prab002 <mprabhanjan18@gmail.com>
2024-08-25 19:13:24 +0530

    added the sidebar functionalty

---
+++ app/(main)/(routes)/document/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Document() {
+  return <div>details lcndlakcdnc dlkc dkcd mck;dm c;dmc </div>;
+}
+
+export default Document;
---
+++ app/(main)/layout.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Spinner } from "@/components/spinner";
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+import Navigation from "./_components/navigation";
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  // Return the spinner while loading
+  if (isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Spinner size={"lg"} />
+      </div>
+    );
+  }
+
+  // Redirect to home if not authenticated
+  if (!isAuthenticated) {
+    redirect("/");
+    return null; // Return null after redirect to avoid further rendering
+  }
+
+  return (
+    <div className="h-full flex dark:bg-[#1f1f1f]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto">{children}</main>
+    </div>
+  );
+};
+
+export default MainLayout;
